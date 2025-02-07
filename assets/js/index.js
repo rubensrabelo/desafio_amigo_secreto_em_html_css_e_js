8f36ee8db11a1b80c4d77c2828c604bc62005a08
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-07 10:54:33 -0300

    Adding btnRandomFriend event to sort a friend from the list

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const inputFriend = document.getElementById("add_friend");
     const btnAdd = document.querySelector("input[type='button']");
     const divResult = document.getElementById("result");
+    const btnRandomFriend = document.getElementById("random_friend");
 
     btnAdd.addEventListener("click", function() {
         const nameFriend = inputFriend.value.trim();
@@ -21,4 +22,20 @@ document.addEventListener("DOMContentLoaded", function() {
             inputFriend.focus();
         }
     });
+
+    btnRandomFriend.addEventListener("click", function() {
+        const listFriends = divResult.querySelectorAll("ul li");
+        const friendNames = Array.from(listFriends).map(friend => friend.textContent);
+
+        if(friendNames.length > 0) {
+            const randomIndex = Math.floor(Math.random() * friendNames.length);
+            const randomFriend = friendNames[randomIndex];
+
+            divResult.innerHTML = "";
+            const resultMessage = document.createElement("p");
+            resultMessage.textContent = `Amigo sorteado: ${randomFriend}`;
+            console.log(resultMessage);
+            divResult.appendChild(resultMessage);
+        }
+    })
 });
